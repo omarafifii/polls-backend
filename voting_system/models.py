65f4919dd3ca5b492bb563849843d1e9eeafcfce
omarafifii <omarafifi@windowslive.com>
2023-09-21 15:19:12 +0300

    Create models

--- voting_system/models.py
+++ voting_system/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class Poll(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    exp_date = models.DateTimeField("Expiry Date")
+
+    class Meta:
+        ordering = ["-exp_date"]
+
+    def __str__(self):
+        return self.title
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.text
+    
+class Voter(models.Model):
+    email = models.EmailField(max_length=200)
+    polls = models.ManyToManyField(Poll)
+
+    def __str__(self):
+        return self.email
+    
+class Confirmation(models.Model):
+    voter = models.ForeignKey(Voter, on_delete=models.CASCADE)
+    otp = models.CharField(max_length=200)
+    isActive = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.voter
